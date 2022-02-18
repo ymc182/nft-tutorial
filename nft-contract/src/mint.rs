caac4ebabc930029384453a9aeadd69e4d85117a
ymc182 <ymc182@gmail.com>
2022-02-18 23:36:48 +1100

    v1

--- nft-contract/src/mint.rs
+++ nft-contract/src/mint.rs
@@ -2,80 +2,102 @@ use crate::*;
 
 #[near_bindgen]
 impl Contract {
-    #[payable]
-    pub fn nft_mint(
-        &mut self,
-        token_id: TokenId,
-        metadata: TokenMetadata,
-        receiver_id: AccountId,
-        //we add an optional parameter for perpetual royalties
-        perpetual_royalties: Option<HashMap<AccountId, u32>>,
-    ) {
-        //measure the initial storage being used on the contract
-        let initial_storage_usage = env::storage_usage();
+	#[payable]
+	pub fn nft_mint(
+		&mut self,
+		//we add an optional parameter for perpetual royalties
+		perpetual_royalties: Option<HashMap<AccountId, u32>>,
+	) {
+		assert!(
+			env::attached_deposit() == 1000000000000000000000000,
+			"Not enough NEAR to mint , mint price is 1 Near"
+		);
+		let receiver_id: AccountId = env::predecessor_account_id();
+		//measure the initial storage being used on the contract
+		let initial_storage_usage = env::storage_usage();
+		let token_id = u128::from(self.nft_total_supply()).to_string();
+		// create a royalty map to store in the token
+		let mut royalty = HashMap::new();
 
-        // create a royalty map to store in the token
-        let mut royalty = HashMap::new();
+		// if perpetual royalties were passed into the function:
+		if let Some(perpetual_royalties) = perpetual_royalties {
+			//make sure that the length of the perpetual royalties is below 7 since we won't have enough GAS to pay out that many people
+			assert!(
+				perpetual_royalties.len() < 7,
+				"Cannot add more than 6 perpetual royalty amounts"
+			);
 
-        // if perpetual royalties were passed into the function: 
-        if let Some(perpetual_royalties) = perpetual_royalties {
-            //make sure that the length of the perpetual royalties is below 7 since we won't have enough GAS to pay out that many people
-            assert!(perpetual_royalties.len() < 7, "Cannot add more than 6 perpetual royalty amounts");
+			//iterate through the perpetual royalties and insert the account and amount in the royalty map
+			for (account, amount) in perpetual_royalties {
+				royalty.insert(account, amount);
+			}
+		}
 
-            //iterate through the perpetual royalties and insert the account and amount in the royalty map
-            for (account, amount) in perpetual_royalties {
-                royalty.insert(account, amount);
-            }
-        }
+		//specify the token struct that contains the owner ID
+		let token = Token {
+			//set the owner ID equal to the receiver ID passed into the function
+			owner_id: receiver_id,
+			//we set the approved account IDs to the default value (an empty map)
+			approved_account_ids: Default::default(),
+			//the next approval ID is set to 0
+			next_approval_id: 0,
+			//the map of perpetual royalties for the token (The owner will get 100% - total perpetual royalties)
+			royalty,
+		};
 
-        //specify the token struct that contains the owner ID 
-        let token = Token {
-            //set the owner ID equal to the receiver ID passed into the function
-            owner_id: receiver_id,
-            //we set the approved account IDs to the default value (an empty map)
-            approved_account_ids: Default::default(),
-            //the next approval ID is set to 0
-            next_approval_id: 0,
-            //the map of perpetual royalties for the token (The owner will get 100% - total perpetual royalties)
-            royalty,
-        };
+		//insert the token ID and token struct and make sure that the token doesn't exist
+		assert!(
+			self.tokens_by_id.insert(&token_id, &token).is_none(),
+			"Token already exists"
+		);
+		let metadata: TokenMetadata = TokenMetadata {
+			copies: None,
+			title: Some(format!("{}{}", "YMC TEST NFT", token_id)),
+			media: Some(format!(
+				"{}{}.png",
+				"https://artplace.mypinata.cloud/ipfs/QmTvmWWasR16DWMsVnVBeaFUB77rikGFCxeQAmdhtSX8JH/",
+				token_id
+			)),
+			description: Some("YMC TEST NFT DESCRIPTION".to_string()),
+			expires_at: None,
+			extra: None,
+			issued_at: None,
+			reference: None,
+			reference_hash: None,
+			starts_at: None,
+			media_hash: None,
+			updated_at: None,
+		};
+		//insert the token ID and metadata
+		self.token_metadata_by_id.insert(&token_id, &metadata);
 
-        //insert the token ID and token struct and make sure that the token doesn't exist
-        assert!(
-            self.tokens_by_id.insert(&token_id, &token).is_none(),
-            "Token already exists"
-        );
+		//call the internal method for adding the token to the owner
+		self.internal_add_token_to_owner(&token.owner_id, &token_id);
 
-        //insert the token ID and metadata
-        self.token_metadata_by_id.insert(&token_id, &metadata);
+		// Construct the mint log as per the events standard.
+		let nft_mint_log: EventLog = EventLog {
+			// Standard name ("nep171").
+			standard: NFT_STANDARD_NAME.to_string(),
+			// Version of the standard ("nft-1.0.0").
+			version: NFT_METADATA_SPEC.to_string(),
+			// The data related with the event stored in a vector.
+			event: EventLogVariant::NftMint(vec![NftMintLog {
+				// Owner of the token.
+				owner_id: token.owner_id.to_string(),
+				// Vector of token IDs that were minted.
+				token_ids: vec![token_id.to_string()],
+				// An optional memo to include.
+				memo: None,
+			}]),
+		};
 
-        //call the internal method for adding the token to the owner
-        self.internal_add_token_to_owner(&token.owner_id, &token_id);
+		// Log the serialized json.
+		env::log_str(&nft_mint_log.to_string());
 
-        // Construct the mint log as per the events standard.
-        let nft_mint_log: EventLog = EventLog {
-            // Standard name ("nep171").
-            standard: NFT_STANDARD_NAME.to_string(),
-            // Version of the standard ("nft-1.0.0").
-            version: NFT_METADATA_SPEC.to_string(),
-            // The data related with the event stored in a vector.
-            event: EventLogVariant::NftMint(vec![NftMintLog {
-                // Owner of the token.
-                owner_id: token.owner_id.to_string(),
-                // Vector of token IDs that were minted.
-                token_ids: vec![token_id.to_string()],
-                // An optional memo to include.
-                memo: None,
-            }]),
-        };
+		//calculate the required storage which was the used - initial
+		let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
 
-        // Log the serialized json.
-        env::log_str(&nft_mint_log.to_string());
-
-        //calculate the required storage which was the used - initial
-        let required_storage_in_bytes = env::storage_usage() - initial_storage_usage;
-
-        //refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
-        refund_deposit(required_storage_in_bytes);
-    }
+		//refund any excess storage if the user attached too much. Panic if they didn't attach enough to cover the required.
+		refund_deposit_address(required_storage_in_bytes, self.owner_id.clone());
+	}
 }
